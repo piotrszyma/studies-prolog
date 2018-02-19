f380f78aa2aa700c1e46ad4f9e873071d8c3521f
Piotr Szyma <thompson2908@gmail.com>
2018-02-19 23:27:05 +0100

    Task 3

---
+++ ps-1/task3.pl
@@ -0,0 +1,18 @@
+above(bicycle, pencil).
+above(camera, butterfly).
+
+left_of(pencil, sandglass).
+left_of(sandglass, butterfly).
+left_of(butterfly, fish).
+
+below(X, Y) :- above(Y, X).
+
+right_of(X, Y) :- left_of(Y, X).
+
+left_of_rec(X, Y) :- left_of(X, Y).
+left_of_rec(X, Y) :- left_of(C, Y), left_of_rec(X, C).
+
+% X wyżej niż Y
+% ponizej X coś jest
+% ponizej Y niczego nie ma
+higher(X, Y) :- (\+ below(_, Y)), below(_, X).
