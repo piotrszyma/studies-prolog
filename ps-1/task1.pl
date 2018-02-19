071080c4e584274b4f2283bb7ef0c451555faf89
Piotr Szyma <thompson2908@gmail.com>
2018-02-19 13:06:54 +0100

    ProblemSet 1 tasks 1, 2

---
+++ ps-1/task1.pl
@@ -0,0 +1,26 @@
+ojciec(zeus,ares).
+ojciec(zeus,dionysus).
+ojciec(cadmus,semelel).
+ojciec(ares,harmonia).
+ojciec(ares,ada).
+ojciec(brat_aresa,stefan).
+
+matka(hera,ares).
+matka(hera,brat_aresa).
+matka(aphrodite,harmonia).
+matka(harmonia,semele).
+matka(semele,dionyslls).
+
+mezczyzna(X) :- ojciec(X, _).
+kobieta(X) :- matka(X, _).
+rodzic(X, Y) :- matka(X, Y); ojciec(X, Y).
+diff(X, Y) :- X \= Y.
+
+jest_matka(X) :- matka(X, _).
+jest_ojcem(X) :- ojciec(X, _).
+jest_synem(X) :- ojciec(_, X).
+
+siostra(X, Y) :- rodzic(Z, X), rodzic(Z, Y), kobieta(X), kobieta(Y).
+brat(X, Y) :- rodzic(Z, X), rodzic(Z, Y), mezczyzna(X), mezczyzna(Y).
+dziadek(X, Y) :- rodzic(Z, Y), ojciec(X, Z).
+rodzenstwo(X, Y) :- rodzic(Z, X), rodzic(Z, Y).
