218ee07350bc73f1df4c0fd1910b80f7aae1bd29
Piotr Szyma <thompson2908@gmail.com>
2018-02-28 18:24:06 +0100

    task 2

---
+++ problem-set-2/task2.pl
@@ -0,0 +1,9 @@
+once(X, X) :- true.
+once(X, [H|T]) :- once(X, H), \+ once(X, T), !.
+once(X, [H|T]) :- \+ once(X, H), once(X, T).
+
+twice(X, X) :- false.
+twice(X, [X, X]) :- true.
+
+twice(X, [H|T]) :- once(X, H), once(X, T), !.
+twice(X, [H|T]) :- \+ once(X, H), twice(X, T).
