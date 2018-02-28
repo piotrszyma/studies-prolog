7fea38833b708f344ca8a28bc96620d59c692d64
Piotr Szyma <thompson2908@gmail.com>
2018-02-28 18:12:11 +0100

    problem-set-2 task1

---
+++ problem-set-2/task1.pl
@@ -0,0 +1,16 @@
+% last of one element list is the element
+% last of list is last of tail of list
+
+last([E], E) :- true.
+last([_|T], E) :- last(T, E).
+
+% E - middle element
+% T - list[1:]
+% X - list[1:-1]
+% L - list[-1]
+
+middle([E], E) :- true.
+middle([_|T], E) :- 
+  append(X, [L], T),
+  middle(X, E), 
+  last(T, L).
