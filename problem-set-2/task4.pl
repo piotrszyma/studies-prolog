4ef807f11765fa132422db78f9289973aa8b011a
Piotr Szyma <thompson2908@gmail.com>
2018-03-04 19:12:41 +0100

    task 4 works, bot not searching

--- problem-set-2/task4.pl
+++ problem-set-2/task4.pl
@@ -1,18 +1,15 @@
 got(josh, car).
 
 gives([[]], josh, car, steve).
-gives([[[]]], steve, car, josh).
+gives([[[[]]]], steve, car, josh).
 
-got([], WHO, WHAT) :- got(WHO, WHAT).
-  
-got([WHEN], WHO, WHAT) :-
-  (
-    gives(WHEN, _, WHAT, WHO),
-    \+ got(WHEN, WHO, WHAT)
-  ).
+got([], WHO, WHAT) :- 
+  got(WHO, WHAT).
 
-got([WHEN], WHO, WHAT) :-
-  (
-    got(WHEN, WHO, WHAT),
-    \+ gives(WHEN, WHO, WHAT, _)
-  ).
+got([EARLIER], WHO, WHAT) :-
+  \+ gives(EARLIER, WHO, WHAT, _),
+  got(EARLIER, WHO, WHAT), !.
+
+got([EARLIER], WHO, WHAT) :-
+  gives(EARLIER, _, WHAT, WHO),
+  \+ got(EARLIER, WHO, WHAT).
