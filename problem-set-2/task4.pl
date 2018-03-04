2bc20575009565acf691585eb76f5634acce8b6c
Piotr Szyma <thompson2908@gmail.com>
2018-03-04 23:03:30 +0100

    working

--- problem-set-2/task4.pl
+++ problem-set-2/task4.pl
@@ -1,6 +1,7 @@
 got(josh, car).
 
 gives([[0]], josh, car, steve).
+% gives([[[0]]], steve, car, john).
 
 got([0], WHO, WHAT) :- 
     got(WHO, WHAT).
@@ -10,5 +11,9 @@ got([EARLIER], WHO, WHAT) :-
   got(EARLIER, GIVER, WHAT).
 
 got([EARLIER], WHO, WHAT) :-
-  got(EARLIER, WHO, WHAT),
-  \+ gives(EARLIER, WHO, WHAT, _).
+  (
+    \+ gives(EARLIER, WHO, WHAT, _),
+    got(EARLIER, WHO, WHAT)
+  ).
+% got(MOMENT, WHO, WHAT) :-
+%   gives(MOMENT, WHO, WHAT, _).
