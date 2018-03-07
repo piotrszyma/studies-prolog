b37b9e445f232365aa7ba458a908b0d0c11f4548
Piotr Szyma <thompson2908@gmail.com>
2018-03-07 18:50:52 +0100

    ps2 task 5

--- problem-set-2/task5.pl
+++ problem-set-2/task5.pl
@@ -1,52 +1,49 @@
 lista(N, XL) :-
-  %       N    C   P   NP   X  
-  lista_n(N, [1], [0], [0],  XL, [0]).
+  %       N   C    P    NP   X  MAX
+  lista_n(N, [1], [0], [0],  XL, 0).
 
 lista_n(N, [C|_], _, _, [], _) :-
+  
   C is 2 * N + 1, !.
 
-lista_n(N, CL, P, NP, [X|XT], ADDED) :-
-  [C|_] = CL,
-  member(X, CL),
-  X =< N,
-  \+ member(X, NP),
-  C1 is C + 1,
-  (
-    % if X < I => it's not its first occurrence
-    (
-      \+ member(X, P), % first occurrence 
-      [BIGGEST|_] = ADDED,
-      X is BIGGEST + 1,
-      ADDED1 = [X|ADDED]
-    );
-    (
-      member(X, P), % second occurrence
-      ADDED1 = ADDED
-    )
-  ),
-  lista_p(N, [C1|CL], P, [X|NP], XT, ADDED1).
+lista_n(N, CL, P, NP, [X|XT], MAX) :-
+  [C|_] = CL,       % extract counter
+  member(X, CL),    % range of X
+  X =< N,           % limit X
+  \+ member(X, NP), % cannot occur on neg pos twice
+  C1 is C + 1,      % increase counter
+  \+ member(X, P),  % CASE: first occurrence 
+  X is MAX + 1,     % if first, increment MAX
+  lista_p(N, [C1|CL], P, [X|NP], XT, X).
 
-lista_p(N, CL, P, NP, [X|XT], ADDED) :-
+
+lista_n(N, CL, P, NP, [X|XT], MAX) :-
+  [C|_] = CL,       % extract counter
+  member(X, CL),    % range of X
+  X =< N,           % limit X
+  \+ member(X, NP), % cannot occur on posit pos twice
+  C1 is C + 1,      % increment counter
+  member(X, P),     % second occurrence, MAX still same
+  lista_p(N, [C1|CL], P, [X|NP], XT, MAX).
+  
+
+lista_p(N, CL, P, NP, [X|XT], MAX) :-
   [C|_] = CL,
   member(X, CL),
   X =< N,
   \+ member(X, P),
   C1 is C + 1,
-  (
-    (
-      \+ member(X, NP), % first occurrence 
-      [BIGGEST|_] = ADDED,
-      X is BIGGEST + 1,
-      ADDED1 = [X|ADDED]
-    );
-    (
-      member(X, NP), % second occurrence
-      ADDED1 = ADDED      
-    )
-  ),
-  lista_n(N, [C1|CL], [X|P], NP, XT, ADDED1).
+  \+ member(X, NP), % first occurrence 
+  X is MAX + 1,
+  lista_n(N, [C1|CL], [X|P], NP, XT, X).    
 
-% jeśli coś się pojawia pierwszy raz
-% to nie ma w tailu 
 
-% i jest już chociaż jeden o jeden mniejszy
+lista_p(N, CL, P, NP, [X|XT], MAX) :-
+  [C|_] = CL,
+  member(X, CL),
+  X =< N,
+  \+ member(X, P),
+  C1 is C + 1,
+  member(X, NP), % second occurrence
+  lista_n(N, [C1|CL], [X|P], NP, XT, MAX).
+  
