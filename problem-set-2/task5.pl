cd2373888f6f76f46802a2a8dd4eafe035e3f48c
Piotr Szyma <thompson2908@gmail.com>
2018-03-07 17:24:37 +0100

    task 5...

--- problem-set-2/task5.pl
+++ problem-set-2/task5.pl
@@ -6,42 +6,48 @@ lista_n(N, [C|_], _, _, []) :-
   C is 2 * N + 1, !.
 
 lista_n(N, CL, P, NP, [X|XT]) :-
-  [PH|_] = P,
   [C|_] = CL,
-  C =< N,
-  member(X, CL),
-  X =< N,
-  X =< PH + 1,
-  \+ member(X, NP),
-  C1 is C + 1,
-  lista_p(N, [C1|CL], P, [X|NP], XT).
-
-lista_n(N, CL, P, NP, [X|XT]) :-
-  [C|_] = CL,
-  C > N,
+  [F|_] = P,
   member(X, CL),
   X =< N,
   \+ member(X, NP),
   C1 is C + 1,
+  (
+    (
+      C =< N,
+      (
+          X is F + 1;
+          X is F
+        )
+    );
+    (
+      C > N
+    )
+  ),
   lista_p(N, [C1|CL], P, [X|NP], XT).
 
-
-lista_p(N, CL, P, NP, [X|XT]) :-
-  [PH|_] = NP,
-  [C|_] = CL,
-  C =< N,
-  member(X, CL),
-  X =< PH + 1,
-  X =< N,
-  \+ member(X, P),
-  C1 is C + 1,
-  lista_n(N, [C1|CL], [X|P], NP, XT).
-
 lista_p(N, CL, P, NP, [X|XT]) :-
   [C|_] = CL,
-  C > N,
+  [F|_] = NP,
   member(X, CL),
   X =< N,
   \+ member(X, P),
   C1 is C + 1,
+  (
+    (
+      C =< N,
+      (
+        X is F + 1;
+        X is F
+      )
+    );
+    (
+      C > N
+    )
+  ),
   lista_n(N, [C1|CL], [X|P], NP, XT).
+
+% jeśli coś się pojawia pierwszy raz
+% to nie ma w tailu 
+
+% i jest już chociaż jeden o jeden mniejszy
