3b803fd8b73254f85881b585749bfd67ad1f4094
Piotr Szyma <thompson2908@gmail.com>
2018-03-16 00:38:17 +0100

    variance done

--- problem-set-3/task1.pl
+++ problem-set-3/task1.pl
@@ -1,10 +1,22 @@
 average(List, Average) :-
   average(List, 0, 0, Average).
 
-average([Head|Tail], Sum, Counter, Average) :-
-  Sum1 is Sum + Head,
-  Counter1 is Counter + 1,
-  average(Tail, Sum1, Counter1, Average).
+average([Head|Tail], Sum, NewCounter, Average) :-
+  NewSum is Sum + Head,
+  NewCounter1 is NewCounter + 1,
+  average(Tail, NewSum, NewCounter, Average).
 
-average([], Sum, Counter, Average) :-
-  Average is Sum / Counter.
+average([], Sum, NewCounter, Average) :-
+  Average is Sum / NewCounter.
+
+variance(List, Variance) :-
+  average(List, Average),
+  variance(List, 0, 0, Average, Variance).
+  
+variance([Head|Tail], Sum, NewCounter, Average, Variance) :-
+  NewSum is Sum + (((Head - Average) * (Head - Average))),
+  NewCounter1 is NewCounter + 1,
+  variance(Tail, NewSum, NewCounter, Average, Variance).
+
+variance([], Sum, NewCounter, _, Variance) :-
+  Variance is Sum / NewCounter.
