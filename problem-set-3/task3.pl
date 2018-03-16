07b92c061ffd5fb56ac4dac53253d544ffab3042
Piotr Szyma <thompson2908@gmail.com>
2018-03-16 13:19:38 +0100

    prolog

--- problem-set-3/task3.pl
+++ problem-set-3/task3.pl
@@ -1,6 +1,16 @@
-permutation(List, [PermHead|PermTail]) :-
-    append(Left, [PermHead|RightTail], List),
+permutation(Available, [PermHead|PermTail]) :-
+    append(Left, [PermHead|RightTail], Available),
     append(Left, RightTail, Remaining),
     permutation(Remaining, PermTail).
 
 permutation([], []).
+
+
+% 1 2 3
+
+%   1   2   3
+%   1   3   2
+%   2   1   3
+%   2   3   1
+%   3   1   2
+%   3   2   1
