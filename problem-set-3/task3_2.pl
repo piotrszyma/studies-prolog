6cda1588fd2d6879380b56f2a9c543a9a73f481e
Piotr Szyma <thompson2908@gmail.com>
2018-03-16 12:59:54 +0100

    classic permutation works

--- problem-set-3/task3_2.pl
+++ problem-set-3/task3_2.pl
@@ -1,6 +1,6 @@
 permutation(List, [PermHead|PermTail]) :-
     append(Left, [PermHead|RightTail], List),
-    append(Left, RightTail, Remaining),
-    permutation(Remaining, PermTail).
+    append(Left, RightTail, Rest),
+    permutation(Rest, PermTail).
 
 permutation([], []).
