2141ba85ed12e5bc3ceeb09b1a9d2947a77af092
Piotr Szyma <thompson2908@gmail.com>
2018-03-17 12:49:14 +0100

    task 3 of ps3 (even/odd permutations)

--- problem-set-3/task3_2.pl
+++ problem-set-3/task3_2.pl
@@ -1,6 +1,24 @@
-permutation(List, [PermHead|PermTail]) :-
-    append(Left, [PermHead|RightTail], List),
-    append(Left, RightTail, Rest),
-    permutation(Rest, PermTail).
+even_permutation( [], [] ).
+even_permutation( [X|T], Perm ) :-
+    even_permutation( T, Perm1 ),
+    insert_odd( X, Perm1, Perm ).
 
-permutation([], []).
+even_permutation( [X|T], Perm ) :-
+    odd_permutation( T, Perm1 ),
+    insert_even( X, Perm1, Perm ).
+
+odd_permutation( [X|T], Perm ) :-
+    odd_permutation( T, Perm1 ),
+    insert_odd( X, Perm1, Perm ).
+
+odd_permutation( [X|T], Perm ) :-
+    even_permutation( T, Perm1 ),
+    insert_even( X, Perm1, Perm ).
+
+insert_odd( X, InList, [X|InList] ).
+insert_odd( X, [Y,Z|InList], [Y,Z|OutList] ) :-
+    insert_odd( X, InList, OutList ).
+
+insert_even( X, [Y|InList], [Y,X|InList] ).
+insert_even( X, [Y,Z|InList], [Y,Z|OutList] ) :-
+    insert_even( X, InList, OutList ).
