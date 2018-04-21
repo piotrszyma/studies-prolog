8f95586a67485210340fc1433b564eec3ece8507
Piotr Szyma <thompson2908@gmail.com>
2018-04-21 14:33:05 +0200

    prolog ps5

--- problem-set-5/task1.pl
+++ problem-set-5/task1.pl
@@ -9,11 +9,34 @@ read_from_stream(InputStream, [CharsHead|CharsTail]) :-
   atom_codes(CharsHead, [CharsCode]),
   read_from_stream(InputStream, CharsTail).
 
+ignore(Symbol) :-
+  member(Symbol, [' ', '\n']).
 
-% key(SŁOWO_KLUCZOWE) read, write, if, then, else, fi, while, do, od, and, or, mod;
+token(Token, key(Token)) :-
+  member(Token, [read, write, if, then, else, fi, while, do, od, and, or, mod]).
+
+token(Token, sep(Token)) :-
+  member(Token, [';', '+', '-', '*', '/', '(', ')', '<', '>', '=<', '>=',':=', '=' ,'/=']).
+
+token(Token, int(Token)) :-
+  integer(Token).
+
+tokenize([], '', []).
+
+tokenize([Symbol|RestOfSymbols], Temporary, [Token|RestOfTokens]) :-
+  atom_concat(Temporary, Symbol, SymbolWithTemporary),
+  token(SymbolWithTemporary, Token),
+  tokenize(RestOfSymbols, '', RestOfTokens).
+
+tokenize([Symbol|RestOfSymbols], Temporary, [Token|RestOfTokens]) :-
+  atom_concat(Temporary, Symbol, SymbolWithTemporary),
+  tokenize(RestOfSymbols, SymbolWithTemporary, [Token|RestOfTokens]).
+
+tokenize([Symbol|RestOfSymbols], Temporary, AllTokens) :-
+  ignore(Symbol),
+  tokenize(RestOfSymbols, Temporary, AllTokens).
+
 
 % int(LICZBA_NATURALNA) liczbą naturalną;
 
 % id(ID) nazwą zmiennej będącą słowem złożonym z wielkich liter;
-
-% sep(SEPARATOR) ’;’, ’+’, ’-’, ’*’, ’/’ ’(’, ’)’, ’<’, ’>’, ’=<’, ’>=’, ’:=’, ’=’, ’/=’
