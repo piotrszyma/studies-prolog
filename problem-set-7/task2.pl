5bdf630e12d671745fbeae9edcd5c79271e5f8ab
Piotr Szyma <thompson2908@gmail.com>
2018-05-27 21:13:31 +0200

    one way ticket to the moon

--- problem-set-7/task2.pl
+++ problem-set-7/task2.pl
@@ -1,23 +1,29 @@
-finish([]) :- true.
-
-head([Head|Tail], Head, Tail).
-
-attach(Element, List, [Element|List]).
 
 split(In, FirstList, SecondList) :-
   split_first(In, FirstList, SecondList).
 
-split_first([], [], []).
 
 split_first(In, FirstList, SecondList) :-
-  head(In, InHead, InTail),
-  attach(InHead, FirstList_, FirstList),
-  split_second(InTail, FirstList_, SecondList).
+  In \== [],
+  freeze(In,
+    (
+      head(In, InHead, InTail),
+      attach(InHead, FirstList_, FirstList),
+      split_second(InTail, FirstList_, SecondList)
+    )
+  ), !.
+
+split_first([], [], []).
 
 split_second(In, FirstList, SecondList) :-
-  head(In, InHead, InTail),
-  attach(InHead, SecondList_, SecondList),
-  split_first(InTail, FirstList, SecondList_).
+  In \== [],
+  freeze(In,
+    (
+      head(In, InHead, InTail),
+      attach(InHead, SecondList_, SecondList),
+      split_first(InTail, FirstList, SecondList_)
+    )
+  ), !.
 
 split_second([], [], []).
 
@@ -25,18 +31,24 @@ split_second([], [], []).
 % Merge sort
 
 
-merge_sort([First, Second], Sorted) :-
+merge_sort([], []) :- 
+  true, !. 
+
+merge_sort([Output|[]], [Output|[]]) :- 
+  true, !. 
+
+merge_sort([First |[Second|[]]], Sorted) :-
   First > Second ->
-    Sorted = [Second, First];
-    Sorted = [First, Second].
+    ( 
+      Sorted = [Second, First], ! 
+    )
+    ;
+    (
+      Sorted = [First, Second], !
+    ).
 
-merge_sort([Single], [Single]).
-
-merge_sort(NotSorted, Sorted) :-
-  length(NotSorted, X),
-  X > 2,
-  split(NotSorted, Left, Right),
+merge_sort([F|[S|[T|R]]], Sorted) :-
+  split([F|[S|[T|R]]], Left, Right),
   merge_sort(Left, SortedLeft),
   merge_sort(Right, SortedRight),
-  merge_(SortedLeft, SortedRight, Sorted).
-
+  merge_(SortedLeft, SortedRight, Sorted), !. 
