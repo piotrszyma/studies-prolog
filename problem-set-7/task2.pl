75b995dde549e002373636beb789f0b4115f4770
Piotr Szyma <thompson2908@gmail.com>
2018-05-28 08:32:17 +0200

    ps7...

--- problem-set-7/task2.pl
+++ problem-set-7/task2.pl
@@ -2,14 +2,18 @@
 split(In, FirstList, SecondList) :-
   split_first(In, FirstList, SecondList).
 
-split_first([], [], []) :- true, !.
-
 split_first(In, FirstList, SecondList) :-
   freeze(In,
     (
-      head(In, InHead, InTail),
-      attach(InHead, FirstList_, FirstList),
-      split_second(InTail, FirstList_, SecondList)
+      length(In, 0) -> (
+      FirstList = [],
+      SecondList = []
+      );
+      (
+        head(In, InHead, InTail),
+        attach(InHead, FirstList_, FirstList),
+        split_second(InTail, FirstList_, SecondList)
+      )
     )
   ), !.
 
@@ -17,42 +21,62 @@ split_first(In, FirstList, SecondList) :-
 split_second(In, FirstList, SecondList) :-
   freeze(In,
     (
-      head(In, InHead, InTail),
-      attach(InHead, SecondList_, SecondList),
-      split_first(InTail, FirstList, SecondList_)
+      length(In, 0) -> (
+        FirstList = [],
+        SecondList = []
+      ); 
+      (
+        head(In, InHead, InTail),
+        attach(InHead, SecondList_, SecondList),
+        split_first(InTail, FirstList, SecondList_)
+      )
     )
   ), !.
 
-split_second([], [], []).
-
-
 % Merge sort
 
-merge_sort([], []). 
+merge_sort(Out, Out_) :-
+  freeze(Out, (
+    Out = [],
+    Out_ = []
+  )). 
+
+merge_sort(Out, Out_) :- 
+  freeze(Out, (
+    length(Out, 1),
+    Out_ = Out
+  )).
 
 merge_sort(List, Sorted) :-
   freeze(List, 
     (
-      List = [First|[Second|[]]],
+    List = [First|[Second|[]]],
+    freeze(Second, (
       First > Second ->
-            ( 
-              Sorted = [Second, First] 
-            )
-            ;
-            (
-              Sorted = [First, Second] 
-            )
-    )
-  ).
+      ( 
+        List = [First|[Second|[]]],
+        Sorted = [Second, First]
+      )
+      ; 
+      (
+        List = [First|[Second|[]]],
+        Sorted = [First, Second] 
+      )
+    ))
+  )).
 
-merge_sort(List, Sorted) :- 
-  freeze(List,
-    (
-      length(List, X),
-      X > 2,
-      split(List, Left, Right),
-      merge_sort(Left, SortedLeft),
-      merge_sort(Right, SortedRight),
-      merge_(SortedLeft, SortedRight, Sorted)
-    )
-  ).
+ merge_sort(List, Sorted) :- 
+   freeze(List,
+     (
+       length(List, X),
+       X > 2,
+       split(List, Left, Right),
+       merge_sort(Left, SortedLeft),
+       merge_sort(Right, SortedRight),
+       merge_(SortedLeft, SortedRight, Sorted)
+     )
+   ).
+
+ ones(L) :-
+  freeze(L, (L=[1|T],ones(T)) ).
+
